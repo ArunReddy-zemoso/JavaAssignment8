1139077522d4803533597b397c045755661583e2
ArunZemoso <arunkumar.marella@zemosolabs.com>
2022-02-02 15:43:00 +0530

    init

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        try{
+            String s=sc.nextLine();
+            if(s.equals("NotNumber")){
+                throw new NotNumberException("Not a valid number");
+            }
+            else if(s.equals("invalidFormat")){
+                throw new InvalidFormatException("Not a valid format");
+            }
+            else
+                throw new InvalidAgeException("Not a valid Age");
+        }catch (NotNumberException | InvalidFormatException | InvalidAgeException e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("Finally");
+        }
+    }
+}
---
+++ src/NotNumberException.java
@@ -0,0 +1,5 @@
+public class NotNumberException extends Exception{
+    public NotNumberException(String s){
+        super(s);
+    }
+}
